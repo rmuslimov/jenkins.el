bff323567473f3250a58e178def72d331621b3fc
Rustem Muslimov <r.muslimov@gmail.com>
2015-07-09 00:33:44 +0300

    Refactor model to use plist as main storage type

--- jenkins.el
+++ jenkins.el
@@ -18,10 +18,10 @@
   "Name of jenkins buffer.")
 
 (defconst jenkins-list-format
-  [("#" 3 f :pad-right 2 :right-align t)
-   ("Name" 35 t)
-   ("Last success" 20 f)
-   ("Last failed" 20 f)]
+  [("#" 3 f :pad-right 2 :right-align t :col-source jenkins--render-indicator)
+   ("Name" 35 t :col-source :name)
+   ("Last success" 20 f :col-source :last-success)
+   ("Last failed" 20 f :col-source :last-failed)]
   "Columns format.")
 
 (defvar jenkins-api-token nil)
@@ -31,20 +31,12 @@
 
 (defvar *jenkins-jobs-list* nil)
 
-(defvar jenkins-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "r") 'jenkins:restart-job)
-    ;; (define-key map (kbd "g") 'butler-refresh)
-    map)
-  "Jenkins status mode keymap.")
+(defun jenkins-jobs-view-url (hostname viewname)
+  "Jenkins url for get list of jobs in queue and their summaries"
+  (format "%sview/%s/api/json?depth=2&tree=name,jobs[name,lastSuccessfulBuild[result,timestamp,duration,id],lastFailedBuild[result,timestamp,duration,id],lastBuild[result]]"
+          hostname viewname))
 
-(defun jenkins:restart-job (&optional jobindex)
-  "Build jenkins job"
-  (interactive)
-  (let (index (tabulated-list-get-id))
-    index))
-
-(defun jenkins:setup-variables ()
+(defun jenkins--setup-variables ()
   "Ask from user required variables if they not defined yet"
   (unless jenkins-hostname
     (setq jenkins-hostname (read-from-minibuffer "Jenkins hostname: ")))
@@ -55,7 +47,51 @@
   (unless jenkins-api-token
     (setq jenkins-api-token (read-from-minibuffer "Jenkins API Token: "))))
 
-(defun jenkins:-parse-time-from (time-since timeitems)
+;; models
+
+(defun jenkins--make-job (name result last-success last-failed)
+  "Define regular jenkins job here."
+  (list :name name
+        :result result
+        :last-success last-success
+        :last-failed last-failed))
+
+(defun jenkins--render-indicator (job)
+  "Special indicator on main jenkins window."
+  (let ((result (plist-get job :result)))
+    (if (equal result "SUCCESS")
+        (propertize "●" 'font-lock-face 'success)
+      (propertize "●" 'font-lock-face 'error))))
+
+(defun jenkins--convert-jobs-to-tabulated-format ()
+  "Use global jenkins-jobs-list prepare data from table"
+  (--map
+   (list
+    (plist-get it :name)
+    (apply 'vector (-map
+     (lambda (column)
+       (let* ((args (nthcdr 3 column))
+              (col-source (plist-get args :col-source)))
+         (if (functionp col-source)
+             (funcall col-source it)
+           (plist-get it col-source))))
+     jenkins-list-format)))
+   *jenkins-jobs-list*
+   ))
+
+;;; actions
+
+(defun jenkins:enter-job (&optional jobindex)
+  (interactive)
+  (message "Enter"))
+
+(defun jenkins:restart-job (&optional jobindex)
+  "Build jenkins job"
+  (interactive)
+  (let (index (tabulated-list-get-id))
+    index))
+
+(defun jenkins--parse-time-from (time-since timeitems)
   "Return list of text representations of time from event"
   (let* ((timeitem (car timeitems))
          (extracted-time (mod time-since (cdr timeitem)))
@@ -64,27 +100,76 @@
     (if (cdr timeitems)
         (apply 'list
                (list extracted-time (car timeitem))
-               (jenkins:-parse-time-from rest-time (cdr timeitems)))
+               (jenkins--parse-time-from rest-time (cdr timeitems)))
       (list (list time-since (car timeitem)))
       )))
 
-(defun jenkins:time-since-to-text (timestamp)
+(defun jenkins--time-since-to-text (timestamp)
   "Returns beatiful string presenting time since event"
   (let* ((timeitems
           '(("s" . 60) ("m" . 60)
             ("h" . 24) ("d" . 1)))
          (seconds-since (- (float-time) timestamp))
-         (time-pairs (jenkins:-parse-time-from seconds-since timeitems))
+         (time-pairs (jenkins--parse-time-from seconds-since timeitems))
          )
     (mapconcat
      (lambda (values) (apply 'format "%d%s" values))
-     (reverse (--filter (not (= (car it) 0)) time-pairs))
+     (-take 3 (reverse (--filter (not (= (car it) 0)) time-pairs)))
      ":")))
 
-(defun jenkins:view-url (hostname viewname)
-  "Jenkins url for get list of jobs in queue and their summaries"
-  (format "%sview/%s/api/json?depth=2&tree=name,jobs[name,lastSuccessfulBuild[result,timestamp,duration,id],lastFailedBuild[result,timestamp,duration,id],lastBuild[result]]"
-          hostname viewname))
+(defun jenkins--refresh-jobs-list ()
+  "Force loading reloading jobs from jenkins and return them formatter for table"
+  (jenkins-get-jobs-list)
+  (jenkins--convert-jobs-to-tabulated-format))
+
+(defun jenkins--parse-jobs-json (data)
+  "Parse gotten data from jenkins, build entries for view"
+  (let ((jobs (cdr (assoc 'jobs data))))
+    (--map
+     (apply 'jenkins--make-job
+      (cdr (assoc 'name it))
+      (cdr (assoc 'result (assoc 'lastBuild it)))
+      (-map
+       (lambda (x)
+         (let ((val (cdr (assoc 'timestamp (assoc x it)))))
+           (if val (jenkins--time-since-to-text (/ val 1000)) "")))
+       (list 'lastSuccessfulBuild 'lastFailedBuild)))
+     jobs)))
+
+(defun jenkins-get-jobs-list ()
+  "Get list of jobs from jenkins server"
+  (let* ((url-request-extra-headers
+          `(("Content-Type" . "application/x-www-form-urlencoded")
+            ("Authorization" .
+             ,(concat "Basic " (base64-encode-string (concat jenkins-username ":" jenkins-api-token))))))
+         (response (with-current-buffer
+                       (url-retrieve-synchronously (jenkins-jobs-view-url jenkins-hostname jenkins-viewname))
+                     (goto-char (point-min))
+                     (re-search-forward "^$")
+                     (delete-region (point) (point-min))
+                     (buffer-string)))
+         (raw-data (json-read-from-string response))
+         (data (jenkins--parse-jobs-json raw-data))
+         )
+    (setq *jenkins-jobs-list* data)))
+
+;; helpers
+
+(defun jenkins:visit-jenkins-web-page ()
+  "Open jenkins web page using predefined variables."
+  (interactive)
+  (unless jenkins-hostname
+    (setq jenkins-hostname (read-from-minibuffer "Jenkins hostname: ")))
+  (browse-url jenkins-hostname))
+
+(defvar jenkins-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "r") 'jenkins:restart-job)
+    (define-key map (kbd "RET") 'jenkins:enter-job)
+    map)
+  "Jenkins status mode keymap.")
+
+;; emacs major mode funcs and variables
 
 (define-derived-mode jenkins-mode tabulated-list-mode "Jenkins"
   "Special mode for jenkins status buffer"
@@ -95,71 +180,19 @@
   (use-local-map jenkins-mode-map)
   (hl-line-mode 1)
   (setq tabulated-list-format jenkins-list-format)
-  (setq tabulated-list-entries 'jenkins:get-jobs-list)
+  (setq tabulated-list-entries 'jenkins--refresh-jobs-list)
   (tabulated-list-init-header)
   (tabulated-list-print))
-
-(defun jenkins:process-data (data)
-  "Parse gotten data from jenkins, build entries for view"
-  (let ((jobs (cdr (assoc 'jobs data))))
-    (-map
-     (lambda (item)
-       (list
-        (cdr (assoc 'name item))
-        (apply 'vector
-               ;; status
-               (if (equal (cdr (assoc 'result (assoc 'lastBuild item))) "SUCCESS")
-                   (propertize "●" 'font-lock-face 'success)
-                 (propertize "●" 'font-lock-face 'error))
-               ;; name
-               (cdr (assoc 'name item))
-               ;; last build times
-               (-map
-                (lambda (x)
-                  (let ((val (cdr (assoc 'timestamp (assoc x item)))))
-                    (if val (jenkins:time-since-to-text (/ val 1000)) "")))
-                (list 'lastSuccessfulBuild 'lastFailedBuild)))))
-                jobs)))
-
-(defun jenkins:get-jobs-list ()
-  "Get list of jobs from jenkins server"
-  (let* ((url-request-extra-headers
-          `(("Content-Type" . "application/x-www-form-urlencoded")
-            ("Authorization" .
-             ,(concat "Basic " (base64-encode-string (concat jenkins-username ":" jenkins-api-token))))))
-         (response (with-current-buffer
-                       (url-retrieve-synchronously (jenkins:view-url jenkins-hostname jenkins-viewname))
-                     (goto-char (point-min))
-                     (re-search-forward "^$")
-                     (delete-region (point) (point-min))
-                     (buffer-string)))
-         (raw-data (json-read-from-string response))
-         (data (jenkins:process-data raw-data))
-         )
-    (progn
-      (setq *jenkins-jobs-list* data)
-      data)
-    ))
-
-(defun jenkins:visit-jenkins-web-page ()
-  "Open jenkins web page using predefined variables."
-  (interactive)
-  (unless jenkins-hostname
-    (setq jenkins-hostname (read-from-minibuffer "Jenkins hostname: ")))
-  (browse-url jenkins-hostname))
-
-(defun jenkins:render ()
-  "Write all information about jenkins jobs."
-  (erase-buffer)
-  (setq buffer-read-only t)
-  (jenkins-mode))
 
 (defun jenkins ()
   "Initialize jenkins buffer."
   (interactive)
-  (jenkins:setup-variables)
+  (jenkins--setup-variables)
   (switch-to-buffer-other-window jenkins-buffer-name)
-  (jenkins:render))
+  (erase-buffer)
+  (setq buffer-read-only t)
+  (jenkins-mode)
+)
 
 (provide 'jenkins)
 ;;; jenkins ends here
